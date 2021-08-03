5bd5f7c52a01e72bfacb57d038d2760e97473873
Syed Rudin <syedrudin04@gmail.com>
2021-08-03 14:44:33 +0800

    Finished off signchange function and related bugs to it. No more bugs as of now.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Select display and buttons
 let display = document.querySelector('.display')
 let keys = document.querySelector('.keys');
 let AC = document.querySelector('[data-action=clear]');
@@ -8,19 +9,26 @@ let divideBtn = document.querySelector('[data-action=divide]');
 let equalsBtn = document.querySelector('[data-action=calculate]')
 let signBtn = document.querySelector('[data-action=signchange]');
 let decimalBtn = document.querySelector('[data-action=decimal]');
+
+// Variables to keep track of calculator state
 let equation = '';
 let ans = false;
 let errorOccured = false;
 
 // Add numbers to display when clicked on
 keys.addEventListener('click', function(e) {
+    // If clicked button is a number or equal to 0
     if ((Number(e.target.textContent)) || (Number(e.target.textContent) === 0)) {
+        // If display is just 0 or there's a previous answer or an error is occuring
         if (display.textContent === '0' || ans || errorOccured) {
+            // Set the display equal to the number pressed
             display.textContent = e.target.textContent;
             equation = e.target.textContent
+            // Set ans and errorOccured variable to false
             ans = false;
             errorOccured = false;
         } else {
+            // Add number onto display and equation
             display.textContent += e.target.textContent;
             equation += e.target.textContent
         }
@@ -29,6 +37,8 @@ keys.addEventListener('click', function(e) {
 
 
 // Define functions for operator keys
+
+// Functionality for addition key
 function addition() {
     if (ans) {
         display.textContent = `${ans}+`;
@@ -45,7 +55,8 @@ function addition() {
         equation += ' + ';
     }
 };
- 
+
+// Functionality for subtraction key
 function subtraction() {
     if (ans) {
         display.textContent = `${ans}-`;
@@ -63,6 +74,7 @@ function subtraction() {
     }
 };
 
+// Functionality for multiplication key
 function multiplication() {
     if (ans) {
         display.textContent = `${ans}x`;
@@ -80,6 +92,7 @@ function multiplication() {
     }
 };
 
+// Functionality for division key
 function division() {
     if (ans) {
         display.textContent = `${ans}÷`;
@@ -93,11 +106,12 @@ function division() {
         return;
     }
     else {
-        display.textContent += '';
+        display.textContent += '÷';
         equation += ' / ';
     }
 };
 
+// Functionality for decimal key
 function decimal() {
     if (ans) {
         display.textContent = '0.';
@@ -106,8 +120,7 @@ function decimal() {
     } else if (display.textContent.slice(-1) === '0') {
         display.textContent += '.';
         equation += '.';
-    }
-    else if (!Number(display.textContent.slice(-1)) || (display.textContent.slice(-1) === '.') || (errorOccured)) {
+    } else if (!Number(display.textContent.slice(-1)) || (display.textContent.slice(-1) === '.') || (errorOccured)) {
         return;
     } else {
         display.textContent += '.'
@@ -125,14 +138,9 @@ function equals() {
         console.error(error);
         display.textContent = 'Error!';
         errorOccured = true;
-        equation = '0';
+        equation = '';
     }  
 }
-
-// TODO
-// 1. Count from right to left
-// 2. Stop when it reaches the end of a number
-// 3. Add brackets around it
 
 function signChange() {
     // let num = true;
@@ -148,21 +156,33 @@ function signChange() {
     //     }
     // }
 
-    // TODO 
-    // Malfunction when sign change in front of operator
-    // Consider regex
     ans = false;
     let liEquation = equation.split(' ');
-    let displayEquation = equation.split(' ');
+    let lastDigit = (liEquation.length-1);
 
-    liEquation.splice(liEquation.length-1, 0, '(-');
+    // Check if last digit is a number or equal to 0
+    if (!Number(liEquation[lastDigit]) || (liEquation[lastDigit] === '0')) {
+        // If it is, do nothing
+        return;
+    }  else {
+        // Check if last digit is a negative number
+        if (Number(liEquation[lastDigit]) < 0) {
+            // If it is, append the negative sign outside of brackets to make it positive
+            liEquation.splice(lastDigit, 0, '-(');
+        } else {
+            // If not, append negative sign within brackets
+            liEquation.splice(lastDigit, 0, '(-');
+        }
+    }
+
+    // Add closing bracket and join back equation
     liEquation.push(')');
-    equation = liEquation.join('');
-
-    displayEquation.splice(displayEquation.length-1, 0, '(-');
-    displayEquation.push(')');
-    displayEquation = displayEquation.join('');
-    display.textContent = displayEquation.replace(/[*]/g, 'x');
+    let newEquation = liEquation.join(' ');
+    let newDisplay = liEquation.join('');
+    
+    equation = newEquation;
+    // Replace all instances of * with x for consistency
+    display.textContent = newDisplay.replace(/[*]/g, 'x');
 }
 
 
@@ -177,7 +197,8 @@ equalsBtn.addEventListener('click', equals);
 
 
 
-// Allow AC button to clear display
+// Allow AC button to clear display and
+// reset all variables
 AC.addEventListener('click', function() {
     display.textContent = '0';
     ans = false;
